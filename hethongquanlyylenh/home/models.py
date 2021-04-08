c474087dcc2a32d38d6eae124169abda8a08c59b
Trinhanh268 <trinhducanh260899@gmail.com>
2021-04-08 22:43:58 +0700

    update hethongquanlyylenh 1.0.2

--- hethongquanlyylenh/home/models.py
+++ hethongquanlyylenh/home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    username = models.CharField(max_length=15)
+    password = models.CharField(max_length=12)
+    doctorname = models.TextField()
