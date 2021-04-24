d75e5e2648493cbb5f8f0ab8f2215ab2f44bae26
Trinhanh268 <trinhducanh260899@gmail.com>
2021-04-24 09:23:53 +0700

    update pdf_view function

--- hethongquanlyylenh/ylenh/urls.py
+++ hethongquanlyylenh/ylenh/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('createylenh/', views.create_ylenh, name="create_ylenh"),
     path('ylenh_delete/<int:id>/', views.ylenh_delete, name="delete_ylenh"),
     path('ylenh_modify/<int:id>/', views.ylenh_modify, name="modify_ylenh"),
+    path('ylenh_detail/<int:id>/', views.ylenh_view_pdf, name="ylenh_detail")
 ]
--- hethongquanlyylenh/ylenh/views.py
+++ hethongquanlyylenh/ylenh/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import YLenh
 from .form import YLenhForm
-from django.http import HttpResponseRedirect 
+from django.http import HttpResponseRedirect , HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 # Create your views here.
 def list(request):
@@ -35,6 +37,22 @@ def ylenh_modify(request, id):
     context = {'form': form}
     return render(request, 'ylenh/ylenh_form.html', context=context)
 
+def ylenh_view_pdf(request, id):
+    ylenh = YLenh.objects.get(id=id)
+    template_path = 'ylenh/ylenh_detail.html'
+    context = {'ylenh': ylenh}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="ylenh.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
 
+    # create a pdf
+    pisa_status = pisa.CreatePDF(html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
 
     
